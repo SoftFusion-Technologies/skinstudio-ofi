98b3bdf2258b39a0051c89d20cee194a9b3c97c3
Benjamin <goosta19802@gmail.com>
2024-09-25 08:31:55 -0300

    Se corrige el mapa

--- src/Components/Mapa.jsx
+++ src/Components/Mapa.jsx
@@ -1,19 +1,20 @@
 import React from 'react';
+
 export default function Mapa() {
   return (
     <div
-      className="flex flex-col md:flex-row lg:flex-row justify-center items-center mx-auto w-full transition-opacity duration-500 "
+      className="flex flex-col md:flex-row lg:flex-row justify-center items-center mx-auto w-full transition-opacity duration-500"
       data-aos="fade-up"
       id="ubicacion"
     >
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d7099.340973373274!2d-65.497462!3d-27.166656!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9423cab5c7a01f6d%3A0x955344507c59c44d!2sCol%C3%B3n%2038%2C%20T4142%20Monteros%2C%20Tucum%C3%A1n!5e0!3m2!1ses!2sar!4v1717859095275!5m2!1ses!2sar"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3537.3108920053286!2d-65.6096806!3d-27.3455997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9423cf71aaa42153%3A0x4781750be79bfb7f!2sCaja%20Popular%202809%2C%20T4146%20Concepci%C3%B3n%2C%20Tucum%C3%A1n!5e0!3m2!1ses!2sar!4v1717859095275!5m2!1ses!2sar"
         width="100%"
         height="450"
         style={{ border: '0' }}
-        allowfullscreen=""
+        allowFullScreen=""
         loading="lazy"
-        referrerpolicy="no-referrer-when-downgrade"
+        referrerPolicy="no-referrer-when-downgrade"
       ></iframe>
     </div>
   );
